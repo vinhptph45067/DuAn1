259e60920bff9d261cf19242ee38d03fb6355b71
TranDuyManh04 <manhtdph45017@fpt.edu.vn>
2024-03-22 13:29:22 +0700

    admin

---
+++ admin/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+    <link rel="stylesheet" href="./assets/css/admin1.css">
+</head>
+
+<body>
+    <div class="boxcenter">
+        <!-- tieu de -->
+        <div class="row mb headeradmin">
+             <h1>Admin</h1>
+        </div>
+        <!-- menu -->
+        <div class="row mb menu">
+             <ul>
+                <li><a href="http://duanmau.test/index.php">Trang chu</a></li>
+                <li><a href="index.php?act=adddm">Danh muc</a></li>
+                <li><a href="index.php?act=addsp">Hang hoa</a></li>
+                <li><a href="index.php?act=dskh">Khach hang</a></li>
+                <li><a href="index.php?act=dsbl">Binh luan</a></li>
+                <li><a href="index.php?act=thongke">Thong ke</a></li>
+                
+
+             </ul>
+        </div>
+        
