b221680552bedce7e982a7d2084e171ad4ebdf59
sonduan1 <sonttph45016@fpt.edu.vn>
2024-04-08 12:42:53 +0700

    giới thiệu

---
+++ view/gioithieu.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nhóm 7 - Website bán trà sữa</title>
+    <link rel="stylesheet" href="../css/gioithieu.css">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <script src="https://kit.fontawesome.com/d829d3809c.js" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+    
+    <br><br><br><br><br><br>
+    <div class="container">
+    <div  class="gioithieu">
+        <h2 style="text-align: center;">Các thông tin giới thiệu về trà sữa ngon và chất lượng  </h2>
+
+        <div  class="conten">
+            <div style="text-align: center;" >
+                <p><b>Tocotoco</b> – Trà sữa ngon</p>
+                <p><b><i>Cho mọi người – Vì mọi người</i></b></p>
+            </div>
+            <div style="text-align: center;" class="conten2">
+                <p> Trà sữa là một loại đồ uống phổ biến xuất phát từ Đài Loan và đã lan rộng ra khắp nhiều nước trên thế giới. Nó được làm từ trà đen, trà xanh hoặc trà oolong kết hợp với sữa tươi và đường. Thành phần chính của trà sữa bao gồm trà, sữa và một loại đường đặc biệt có tên là "boba" hoặc "trân châu" - những viên củi nhỏ và mềm màu đen.
+
+Trà sữa có nhiều biến thể và hương vị khác nhau, tùy thuộc vào sở thích cá nhân và các thành phần phụ khác như trái cây, pudding, kem, hạt trân châu, khoai môn, thạch, nha đam, matcha, chocolate, và nhiều hương vị khác. Nó thường được phục vụ trong các cửa hàng trà sữa hoặc quán cà phê, và có thể được thưởng thức với ống hút đặc biệt để hấp thụ trân châu khi uống.
+
+Trà sữa đã trở thành một trào lưu phổ biến và được ưa chuộng bởi vị ngon, hương thơm đặc trưng của trà cùng với độ ngọt và kem của sữa.
+                    <br>
+                </p>
+            </div>
+            <div style="text-align: center;">
+                <img src="./view/images/13.jpg" alt="">
+            </div>
+            <div class="conten2">
+                <p> Trà sữa là một loại đồ uống phổ biến được nhiều người yêu thích. Nó có những lợi ích sau đây:
+                    <br><br>
+                    - Cung cấp năng lượng: Trà sữa thường chứa đường và các nguồn năng lượng từ sữa và trà. Điều này giúp cung cấp năng lượng nhanh chóng và tạo cảm giác tỉnh táo.
+                    <br><br>
+                    - Cung cấp canxi: Sữa là thành phần chính trong trà sữa, cung cấp canxi quan trọng cho sự phát triển và duy trì sức khỏe của xương và răng.
+                    <br><br>
+                    - Tạo cảm giác thư giãn: Việc thưởng thức trà sữa có thể là một trải nghiệm thư giãn và giúp giảm căng thẳng. Trong một số trường hợp, hương thơm của trà cũng có thể có tác dụng thư giãn và làm dịu tâm trạng.
+                    <br><br>
+                   
+                </p>
+            </div>
+            <div>
+                <img src="images/conten1.jpg" alt="">
+            </div>
+            <div class="conten2">
+                <p>Mọi thông tin chi tiết vui lòng liên hệ đến:
+                    <br><br>
+                    <b>Số điện thoại:</b> 0988002157
+                    <br><br>
+                    <b>Fanpage: </b> Tocotoco
+                    <br><br>
+                    <b>Địa chỉ:</b> Số nhà 186, Phúc Diễn, Xuân Phương, Quận Nam Từ Liêm, Thành Phố Hà Nội
+                    <br>
+                </p>
+            </div>
+
+        </div>
+      
+        </div>
+    </div>
+        
