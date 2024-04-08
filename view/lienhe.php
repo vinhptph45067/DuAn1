6e69e069ce6a575d5a6d6ee21c75ea3203836b30
vinhptph45067 <vinhptph45067@fpt.edu.vn>
2024-04-08 12:55:08 +0700

    Liên hệ

---
+++ view/lienhe.php
@@ -0,0 +1,22 @@
+<br><br><br><br><br><br><br>
+
+
+<div class="container" style="display: flex;column-gap: 20px;">
+<div class="container">
+    <h2>Liên hệ</h2>
+    <img width="250px" src="./view/images/2.jpg" alt=""> 
+    <br> <br> <br>
+    <b>Email:</b> sonttph45016@fpt.edu.vn
+    <br><br>
+    <b>Số điện thoại:</b> 0988002157
+    <br><br>
+    <b>Fanpage: </b> Tocotoco
+    <br><br>
+    <b>Địa chỉ:</b> Số nhà 186, Phúc Diễn, Xuân Phương, Quận Nam Từ Liêm, Thành Phố Hà Nội
+    <br>
+    </p>
+</div>
+<div class="container">
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.9166550636605!2d105.75561127460081!3d21.036020587530487!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31345495842278f5%3A0xdb7907e785ddab28!2zMTg2IFBow7pjIERp4buFbiwgWHXDom4gUGjGsMahbmcsIE5hbSBU4burIExpw6ptLCBIw6AgTuG7mWksIFZp4buHdCBOYW0!5e0!3m2!1svi!2s!4v1711694559753!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" width=" 900" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+</div>
